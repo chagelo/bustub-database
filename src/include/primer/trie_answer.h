c7f9ca08450f1f07c01fc3ee760bdabebd784d51
chagelo <changeto104@gmail.com>
2023-04-26 10:31:38 +0800

    update project0 task1

--- src/include/primer/trie_answer.h
+++ src/include/primer/trie_answer.h
@@ -69,47 +69,70 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
     temp = root_->Clone();
   }
 
+  auto oldcur = std::const_pointer_cast<TrieNode>(root_);
   std::shared_ptr<TrieNode> cur(std::move(temp));
   std::shared_ptr<TrieNode> pre(nullptr);
   std::shared_ptr<TrieNode> root(cur);
 
   int n = key.size();
-  for (int i = 0; i < n - 1; ++i) {
-    if (!cur->children_.count(key[i])) {
-      // for current node, clone a new node and create a new children
-      // then change the children of current parrent to current new clone node
-      if (i != 0) {
-        cur = std::shared_ptr<TrieNode>(cur->Clone());
-        pre->children_[key[i - 1]] = cur;
+  for (int i = 0; i < n; ++i) {
+    if (i != 0) {
+      if (oldcur) {
+        // otherwise, copy the oldversion node (and its children)        
+        cur = std::shared_ptr<TrieNode>(oldcur->Clone());
+        
+        // down to search the key[i];
+        if (oldcur->children_.count(key[i])) {
+          oldcur = std::const_pointer_cast<TrieNode>(oldcur->children_[key[i]]);
+        } else {
+          oldcur = nullptr;
+        }
+      } else {
+        // the correspond old verision node is nullptr, just create a new node
+        cur = std::make_shared<TrieNode>();
+
       }
-      cur->children_[key[i]] = std::make_shared<TrieNode>();
+
+      pre->children_[key[i - 1]] = cur;
+      pre = cur;
+
+    } else {
+      if (oldcur) {
+        if (oldcur->children_.count(key[i])) {
+          oldcur = std::const_pointer_cast<TrieNode>(oldcur->children_[key[i]]);
+        } else {
+          oldcur = nullptr;
+        }
+      }
+      pre = cur;
     }
-
-    // search down
-    pre = cur;
-    cur = std::const_pointer_cast<TrieNode>(cur->children_[key[i]]);
   }
 
-  if (n != 1) {
-    cur = std::shared_ptr<TrieNode>(cur->Clone());
-    pre->children_[key[n - 2]] = cur;
-  }
 
-  auto vptr = std::make_shared<T>(value);
+  // first copy children, then call TrieNodeWithValue(children_, value)
+  if (oldcur) {
+    // copy children
+    cur = std::shared_ptr<TrieNode>(oldcur->Clone());
 
-  if (!cur->children_.count(key[n - 1])) {
-    // spectial for last node
-    cur->children_[key[n - 1]] = std::make_shared<TrieNodeWithValue<T>>(vptr);
   } else {
-    auto curchildren = cur->children_[key[n - 1]]->Clone();
-    cur->children_[key[n - 1]] =
-        std::make_shared<TrieNodeWithValue<T>>(curchildren->children_, vptr);
+    // else create a new node with no children
+    cur = std::make_shared<TrieNode>();
+
   }
+  auto vptr = std::make_shared<T>(value);
+  pre->children_[key[n - 1]] = std::make_shared<TrieNodeWithValue<T>>(cur->children_, vptr);
 
   return Trie(root);
 }
 
 auto Trie::Remove(std::string_view key) const -> Trie {
+  /* 
+  if cannot find the key, return this
+    1. root is nullptr
+    2. cannot the node with the given key
+    3. the found node is not a value node
+  */
+
   // for delete, one way is to set is_value_node_ to false;
   if (root_ == nullptr) return *this;
 
@@ -122,43 +145,33 @@ auto Trie::Remove(std::string_view key) const -> Trie {
     }
   }
 
+  auto oldcur = std::const_pointer_cast<TrieNode>(root_);
   std::shared_ptr<TrieNode> cur(root_->Clone());
   std::shared_ptr<TrieNode> pre(nullptr);
   std::shared_ptr<TrieNode> root(cur);
 
   int n = key.size();
 
-  for (int i = 0; i < n - 1; ++i) {
-    if (cur == nullptr or !cur->children_.count(key[i])) return *this;
+  for (int i = 0; i < n; ++i) {
+    // not find the key to be removed, return current trie
+    if (!oldcur or !oldcur->children_.count(key[i])) return *this;
+
+    // for root, we have create a node for root
+    if (i != 0) {
+      cur = std::shared_ptr<TrieNode>(oldcur->Clone());
+      pre->children_[key[i - 1]] = cur;
+    }
     pre = cur;
-    cur = std::const_pointer_cast<TrieNode>(cur->children_[key[i]]);
+    oldcur = std::const_pointer_cast<TrieNode>(oldcur->children_[key[i]]);
   }
 
-  if (cur->children_.count(key[n - 1])) {
-    // the value is not exist, just return *this
-    if (!cur->children_[key[n - 1]]->is_value_node_) {
-      return *this;
-    }
+  if (!oldcur->is_value_node_) return *this;
 
-    // new children has the same children as the old
-    // just children of cur->children_[key[n - 1]]
-    auto curchildren = cur->children_[key[n - 1]]->Clone();
-    auto newchildren = std::make_shared<TrieNode>(curchildren->children_);
+  // first copy children, then set parent
+  cur = std::make_shared<TrieNode>(oldcur->Clone()->children_);
+  pre->children_[key[n - 1]] = cur;
 
-    // key has length 1
-    // set the the son of parent to the cur node
-    if (n != 1) {
-      auto newcur = cur->Clone();
-      cur = std::shared_ptr<TrieNode>(std::move(newcur));
-      pre->children_[key[n - 2]] = cur;
-    }
-
-    // set the son of cur node to new children
-    cur->children_[key[n - 1]] = newchildren;
-    return Trie(root);
-  } else {
-    return *this;
-  }
+  return Trie(root);
 }
 
 const uint32_t CASE_1_YOUR_ANSWER = 0;
