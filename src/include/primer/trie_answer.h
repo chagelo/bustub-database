c7e5365322003b68b22cf941e50c67a989c3c846
chagelo <changeto104@gmail.com>
2023-04-27 19:06:56 +0800

    fix trie_debug_test.cpp

--- src/include/primer/trie_answer.h
+++ src/include/primer/trie_answer.h
@@ -1,5 +1,5 @@
 #include "primer/trie.h"
 
-const uint32_t CASE_1_YOUR_ANSWER = 8;
-const uint32_t CASE_2_YOUR_ANSWER = 1;
-const uint32_t CASE_3_YOUR_ANSWER = 42;
+const uint32_t CASE_1_YOUR_ANSWER = 7;
+const uint32_t CASE_2_YOUR_ANSWER = 2;
+const uint32_t CASE_3_YOUR_ANSWER = 30;
