12a1bd6fe7202d964e063156ca0d69ade186802d
chagelo <changeto104@gmail.com>
2024-01-16 22:56:05 +0800

    impl left join, inner join

--- src/include/execution/executors/nested_loop_join_executor.h
+++ src/include/execution/executors/nested_loop_join_executor.h
@@ -55,6 +55,13 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
  private:
   /** The NestedLoopJoin plan node to be executed. */
   const NestedLoopJoinPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> left_executor_;
+  std::unique_ptr<AbstractExecutor> right_executor_;
+  Tuple left_tuple_{};
+  Tuple right_tuple_{};
+  bool outer_not_end_{true};
+  bool inner_not_end_{true};
+  bool find_{false};
 };
 
 }  // namespace bustub
