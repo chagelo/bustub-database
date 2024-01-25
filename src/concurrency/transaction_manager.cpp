7ba8459ade35d37b44274a5f8308a5d99ac1d774
chagelo <changeto104@gmail.com>
2024-01-25 15:01:23 +0800

    impl transaction abort

--- src/concurrency/transaction_manager.cpp
+++ src/concurrency/transaction_manager.cpp
@@ -19,6 +19,7 @@
 
 #include "catalog/catalog.h"
 #include "common/macros.h"
+#include "concurrency/transaction.h"
 #include "storage/table/table_heap.h"
 namespace bustub {
 
@@ -27,18 +28,57 @@ void TransactionManager::Commit(Transaction *txn) {
   ReleaseLocks(txn);
 
   txn->SetState(TransactionState::COMMITTED);
+  std::lock_guard lock(txn_map_mutex_);
+  txn_map_.erase(txn->GetTransactionId());
 }
 
 void TransactionManager::Abort(Transaction *txn) {
-  /* TODO: revert all the changes in write set */
+  // update tuple meta
+  auto table_write_records = txn->GetWriteSet();
+  while (!table_write_records->empty()) {
+    auto &record = table_write_records->back();
+    auto tuple_meta = record.table_heap_->GetTupleMeta(record.rid_);
+    switch (record.wtype_) {
+      case WType::INSERT:
+        tuple_meta.is_deleted_ = true;
+        record.table_heap_->UpdateTupleMeta(tuple_meta, record.rid_);
+        break;
+      case WType::DELETE:
+        tuple_meta.is_deleted_ = false;
+        record.table_heap_->UpdateTupleMeta(tuple_meta, record.rid_);
+        break;
+      case WType::UPDATE:
+        break;
+    }
+    table_write_records->pop_back();
+  }
+
+  // update tuple index meta
+  auto index_write_records = txn->GetIndexWriteSet();
+  while (!index_write_records->empty()) {
+    auto &record = index_write_records->back();
+    switch (record.wtype_) {
+      case WType::INSERT:
+        record.catalog_->GetIndex(record.index_oid_)->index_->DeleteEntry(record.tuple_, record.rid_, txn);
+        break;
+      case WType::DELETE:
+        record.catalog_->GetIndex(record.index_oid_)->index_->InsertEntry(record.tuple_, record.rid_, txn);
+        break;
+      case WType::UPDATE:
+        break;
+    }
+    index_write_records->pop_back();
+  }
 
   ReleaseLocks(txn);
 
   txn->SetState(TransactionState::ABORTED);
+  std::lock_guard lock(txn_map_mutex_);
+  txn_map_.erase(txn->GetTransactionId());
 }
 
 void TransactionManager::BlockAllTransactions() { UNIMPLEMENTED("block is not supported now!"); }
 
-void TransactionManager::ResumeTransactions() { UNIMPLEMENTED("resume is not supported now!"); }
+void TransactionManager::ResumeTransactions() {}
 
 }  // namespace bustub
